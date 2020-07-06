d9a968d6f158b9a1440681124eac6bff7e3da918
Alex Suvorov <alex.surkis@gmail.com>
2020-07-06 14:50:37 +0300

    - add Project

---
+++ TestTaskCurrencies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestTaskCurrencies
+//
+//  Created by Vadim Albul on 06.07.2020.
+//  Copyright © 2020 Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
